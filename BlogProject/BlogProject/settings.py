f07caa102de3b943b3d9a000ff4874e56e72f8ba
Shalu BrainerHub <142219819+shalugbrainerhub@users.noreply.github.com>
2023-08-25 15:51:32 +0530

    Update settings.py

--- BlogProject/BlogProject/settings.py
+++ BlogProject/BlogProject/settings.py
@@ -107,7 +107,7 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 EMAIL_HOST_USER = 'shalug.brainerhub@gmail.com'  
-EMAIL_HOST_PASSWORD = 'yolbcgzejyvkodjv' 
+EMAIL_HOST_PASSWORD = 'YOURPASSCODE' 
 
 
 
